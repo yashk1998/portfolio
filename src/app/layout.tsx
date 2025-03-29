799ee691c15359442318397be2938f5b253e16d5
toukoum <raphaelgiraud12@gmail.com>
2025-03-29 15:00:23 +0100

    add logs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
@@ -86,6 +87,7 @@ export default function RootLayout({
           </main>
           <Toaster />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
