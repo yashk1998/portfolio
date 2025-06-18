7271a703c580b43d5cf6dd5d2d7fe4e2b30171d9
toukoum <raphaelgiraud12@gmail.com>
2025-06-18 12:04:26 +0200

    refine height

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -382,7 +382,7 @@ const Chat = () => {
         </div>
 
         {/* Fixed Bottom Bar */}
-        <div className="sticky bottom-0 bg-white pt-3 px-2 md:px-0 pb-4">
+        <div className="sticky bottom-0 bg-white pt-3 px-2 md:px-0 md:pb-4">
           <div className="relative flex flex-col items-center gap-3">
             <HelperBoost submitQuery={submitQuery} setInput={setInput} />
             <ChatBottombar
