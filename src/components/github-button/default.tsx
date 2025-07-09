3dee58f8082025b03b442406456689cb292e2e3f
toukoum <raphaelgiraud12@gmail.com>
2025-07-09 11:01:45 +0200

    fix api key and add .env.example

--- src/components/github-button/default.tsx
+++
@@ -1,9 +0,0 @@
-import { GithubButton } from '@/components/ui/github-button';
-
-export default function Component() {
-  return (
-    <div className="space-y-4">
-      <GithubButton targetStars={1501} label="Star on GitHub" repoUrl="https://github.com/keenthemes/reui" />
-    </div>
-  );
-}
